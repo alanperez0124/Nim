e2892f6d7db135e80d4c3676c251bbb7882046bc
Alan Perez <perezalan012400@gmail.com>
2021-01-21 15:20:55 -0800

    Added get_move method to human and random bot; more basic gameplay mechanics

--- nim.py
+++ nim.py
@@ -6,26 +6,29 @@ first_names = [line.split(" ")[0] for line in names]
 class Player:
     def __init__(self, name):
         """Player Name"""
-        self.human = rf"{name}"
+        self.human = name
     
-    def get_move(self, game):
-        pass
+    def get_move(self):
+        return input("Input how many coins you'd like to take away: ")
 
 
 class RandomComputerPlayer:
     def __init__(self):
         self.bot_name = random.choice(first_names)
+        print(rf'You will be playing against {self.bot_name}')
         
-    def get_random_move(self, game):
-        pass
+    def get_move(self):
+        return random.randint(1, 3)
     
 
 class GeniusComputerPlayer:
     def __init__(self):
         self.bot_name = random.choice(first_names)
+        print(rf'You will be playing against {self.bot_name}')
 
-    def intelligent_move(self, game):
-        pass
+    def get_move(self):
+        return random.randint(1, 3)
+
 
 
 class Gameplay:
@@ -35,13 +38,30 @@ class Gameplay:
 
     def print_coins(self):
         print(" o" * (self.coins - self.taken))
+
+
         
 
     
-
-def play(game, x, y, print_game):
+turn = 'p1'
+def play(game, p1, p2, print_game):
     if print_game:
          game.print_coins()
+    
+    while game.coins > 0:
+        if turn == 'p1':
+            take_away = p1.get_move()
+            print(take_away)
+
+
+            turn == 'p2'
+            
+        else:
+            take_away = p2.get_move()
+        
+
+
+
 
     
 
